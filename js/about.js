4cc189a2ebd25f666c027160e3e77c38cc2b0032
Felicia <190894036+feliJS@users.noreply.github.com>
2026-09-24 21:01:47 +0200

    Start of abou

--- js/about.js
+++ js/about.js
@@ -4,12 +4,11 @@ export function renderAbout() {
 
     app.innerHTML = `
         <section class="content-page">
-
             <h1>Om oss</h1>
-
             <p>
                 Här kommer information om TräningsCentret.
             </p>
+            <div class="common-divider"></div>
 
         </section>
     `;
